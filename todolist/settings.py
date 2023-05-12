3b8f37121c37cf94aa2b808d5cc77a88d2654ad3
mkN <mknord1565@gmail.com>
2023-05-12 23:27:09 +0300

    test deploy

--- todolist/settings.py
+++ todolist/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2g$w_uj@vz(r@p55^reba=1=-s^!w8s@ud-yd*_b!g$+^11-hm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(strtobool(os.getenv('DEBUG')))
+# DEBUG = bool(strtobool(os.getenv('DEBUG')))
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
