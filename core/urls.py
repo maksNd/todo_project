af1fd4e92a408143b26588a387e79d8fd3095c38
mkN <mknord1565@gmail.com>
2023-05-18 00:06:04 +0300

    lesson 35 fix

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from core import views
 
 urlpatterns = [
     path('signup', views.RegistrationView.as_view(), name='signup'),
-    path('login', views.RegistrationView.as_view(), name='login'),
-    path('profile', views.RegistrationView.as_view(), name='profile'),
-    path('update_password', views.RegistrationView.as_view(), name='update_password'),
+    path('login', views.LoginView.as_view(), name='login'),
+    path('profile', views.ProfileView.as_view(), name='profile'),
+    path('update_password', views.UpdatePasswordView.as_view(), name='update_password'),
 ]
